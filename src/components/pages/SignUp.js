9c1e9c8b6960bb89569735b3340f45232f24c357
Prakash <bhanudevflow@gmail.com>
2025-03-13 00:58:17 -0500

    added signup

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,119 @@
-import React from 'react';
-import '../../App.css';
+// import React from 'react';
+// import '../../App.css';
 
-export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+// export default function SignUp() {
+//   return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+// }
+
+import React, { useState } from "react";
+import axios from "axios";
+
+function SignUp() {
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    phonenumber: "",
+    email: "",
+    password: "",
+    profile_picture: null,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleFileChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      profile_picture: e.target.files[0],
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formDataToSend = new FormData();
+    for (let key in formData) {
+      formDataToSend.append(key, formData[key]);
+    }
+
+    try {
+      const response = await axios.post("http://localhost:5000/signup", formDataToSend, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      alert(response.data);
+    } catch (error) {
+      console.error("There was an error!", error);
+      alert("Error signing up!");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Create an Account</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="firstname"
+          placeholder="First Name"
+          value={formData.firstname}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="lastname"
+          placeholder="Last Name"
+          value={formData.lastname}
+          onChange={handleChange}
+          required
+        />
+        <select
+          name="country_code"
+          onChange={handleChange}
+          required
+        >
+          <option value="+1">+1 (USA)</option>
+          <option value="+91">+91 (India)</option>
+          <option value="+44">+44 (UK)</option>
+        </select>
+        <input
+          type="text"
+          name="phonenumber"
+          placeholder="Phone Number"
+          value={formData.phonenumber}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="file"
+          name="profile_picture"
+          onChange={handleFileChange}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 }
+
+export default SignUp;
